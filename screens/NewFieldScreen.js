20e464d4f2fa4afa233b2daf16d653ab67799fe7
carlosdaniel.drury <carlosdaniel.drury@ifood.com.br>
2020-04-10 19:53:34 -0300

    Add call to Fields api. Add error and sucess handler

--- screens/NewFieldScreen.js
+++ screens/NewFieldScreen.js
@@ -1,33 +1,69 @@
-import React from 'react';
-import { View, Picker, StyleSheet, TextInput } from 'react-native';
+import React, { Component, Fragment } from 'react';
+import { View, Picker, StyleSheet, TextInput, Text, Button, ActivityIndicator } from 'react-native';
 import { API_ABSOLUTE_PATH } from '../config/Config'
+import * as yup from 'yup'
+import { Formik } from 'formik'
+
 
 export default class NewFieldScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: false, hasAnyErrors: false, success: false };
+    this.state = { 
+      isLoading: false, 
+      hasAnyErrors: false, 
+      success: false, 
+    };
+    this.submit = this.submit.bind(this)
+    this.postData = this.postData.bind(this)
   }
 
-  postData = async(str) => {
+  async postData(str) {
     try {
+      this.setState({ success: false,
+        hasAnyErrors: false,
+        isLoading: true 
+      })
       let res = await fetch(API_ABSOLUTE_PATH, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          str,
-        }),
+        body: JSON.stringify(str),
       });
+      if (!res.ok) {
+        this.setState({ 
+          hasAnyErrors: true,
+          success: false,
+          isLoading: false
+         });
+      } else {
+        this.setState({ 
+          success: true,
+          hasAnyErrors: false,
+          isLoading: false
+         })
+      }
       res = await res.json();
     } catch (e) {
+      this.setState({ 
+        success: true,
+        hasAnyErrors: false,
+        isLoading: false
+       })
       console.error(e);
     }
   }
 
-  async componentDidMount() {
-    await this.postData("")
+  submit = values => {
+      let requestBody = [
+        {
+          "name" : values.name,
+          "cropType": values.cropType,
+          "area": values.area
+        }
+      ]
+      this.postData(requestBody)
   }
 
   render() {
@@ -53,39 +89,82 @@ export default class NewFieldScreen extends React.Component {
           <Text>Success! Field has been created on the server!</Text>
           <Text 
               onPress={() => {
-                //this.setState = { success = false };
+                this.setState({ success: false });
               }}>Create a new one!</Text>
         </View>
       )
     }
 
     return (
-      <View>
-        <TextInput 
-          placeholder="Name" />
-          <Picker selectedValue={cropTypes.cotton.title}>
-              <Picker.Item label={ cropTypes.cotton } value={ cropTypes.cotton.title }/>
-              <Picker.Item label={ cropTypes.maize } value={ cropTypes.maize.title }/>
-              <Picker.Item label={ cropTypes.oats } value={ cropTypes.oats.title }/>
-              <Picker.Item label={ cropTypes.wheat } value={ cropTypes.wheat.title }/>
-          </Picker>
-          <TextInput
-            placeholder="Area" />
-      </View>
+      <Formik
+        initialValues={{name: "", cropType: cropTypes[0], area: ""}}
+        onSubmit={values => this.submit(values)}
+        validationSchema={yup.object().shape({
+          name: yup
+                .string("Name")
+                .min(2, "At least 2 caracters")
+                .required(),
+          cropType: yup
+                .string()
+                .required(),
+          area: yup
+                .number("area")
+                .max(1000, "min value 1 and max value 1000")
+                .min(1, "min value 1 and max value 1000")
+                .required()
+
+        }) }
+      >
+        {({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
+          <Fragment>
+            <TextInput
+              value= {values.name}
+              onChangeText={handleChange('name')}
+              onBlur={() => setFieldTouched('name')}
+              placeholder="Name"
+            />
+            {touched.name && errors.name &&
+              <Text style={{ fontSize: 10, color: 'red' }}>{errors.name}</Text>
+            }
+            <Picker 
+            value= {values.cropType} 
+            selectedValue = { values.cropType }
+            onValueChange={handleChange('cropType')}>
+                {
+                  cropTypes.map( (v) => {
+                    return <Picker.Item key={v} label={v.toUpperCase()} value={v} />
+                  })
+                }
+            </Picker>
+
+            <TextInput
+              value={values.area}
+              onChangeText={handleChange('area')}
+              placeholder="Area"
+              onBlur={() => setFieldTouched('area')}
+            />
+            {touched.area && errors.area &&
+              <Text style={{ fontSize: 10, color: 'red' }}>{errors.area}</Text>
+            }
+            <Button
+              title='Send'
+              disabled={!isValid}
+              onPress={handleSubmit}
+            />
+          </Fragment>
+        )}
+      </Formik>
     );
   }
 }
 
-const cropTypes = {
-  cotton: "Cotton",
-  maize: "Maize",
-  oats: "Oats",
-  wheat: "Wheat"
-}
-
 const styles = StyleSheet.create ({
-
+  
 });
+
+const cropTypes = [
+  "cotton", "maize", "oats", "wheat"
+]
 
 NewFieldScreen.navigationOptions = {
   title: 'New Field',
